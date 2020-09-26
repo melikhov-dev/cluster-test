c9e7cec9f8a527c6af2c9551de11dd023f5401c2
Martín Pielvitori <martinpielvitori@gmail.com>
2020-09-26 20:12:02 -0300

    feat: add express method just to compare against the basic one

--- express/server/server.js
+++ express/server/server.js
@@ -1,15 +1,17 @@
 const http = require('http');
 const url = require('url');
+const express = require('express');
 const bench = require('./bench');
 
 const host = '0.0.0.0';
 const port = 8000;
 const defaultInputValue = 500;
-
-http.createServer((request, response) => {
+const app = express();
+app.get('/server.js', (request, response) => {
   const inputValue = url.parse(request.url, true).query.input;
   bench(inputValue ? Number(inputValue) : defaultInputValue);
   response.end(JSON.stringify('OK'));
-}).listen(port, host, () => {
+});
+http.createServer(app).listen(port, host, () => {
   console.log(`Node.js Standard Library HTTP server running on port: ${port}`);
 });
